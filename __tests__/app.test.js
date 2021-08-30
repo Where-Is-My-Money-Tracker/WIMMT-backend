2cbac10632fd1e4d74c19b1c6f445fa988c99a41
Pete Hamrick <pete.hamrick@gmail.com>
2021-08-30 11:48:55 -0700

    got rid of animals test route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -15,9 +15,9 @@ describe('app routes', () => {
   
       await client.connect();
       const signInData = await fakeRequest(app)
-        .post('/auth/signup')
+        .post('/auth/signin')
         .send({
-          email: 'jon@user.com',
+          email: 'john@arbuckle.com',
           password: '1234'
         });
       
@@ -28,35 +28,6 @@ describe('app routes', () => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
-
-      const expectation = [
-        {
-          'id': 1,
-          'name': 'bessie',
-          'cool_factor': 3,
-          'owner_id': 1
-        },
-        {
-          'id': 2,
-          'name': 'jumpy',
-          'cool_factor': 4,
-          'owner_id': 1
-        },
-        {
-          'id': 3,
-          'name': 'spot',
-          'cool_factor': 10,
-          'owner_id': 1
-        }
-      ];
-
-      const data = await fakeRequest(app)
-        .get('/animals')
-        .expect('Content-Type', /json/)
-        .expect(200);
-
-      expect(data.body).toEqual(expectation);
-    });
+    
   });
 });
