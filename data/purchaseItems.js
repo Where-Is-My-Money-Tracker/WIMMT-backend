0f74d30069ab97c1b45467a683e3022d459afd78
Pete Hamrick <pete.hamrick@gmail.com>
2021-09-02 22:56:56 -0700

    added new seed data

--- data/purchaseItems.js
+++ data/purchaseItems.js
@@ -40,7 +40,7 @@ module.exports = [
     cost: 75.00
   },
   {
-    id: 6,  
+    id: 6,
     user_id: 1,
     description: 'Karaoke Machine',
     category_id: 10,
@@ -54,5 +54,157 @@ module.exports = [
     category_id: 10,
     timestamp: 1630297328170,
     cost: 129.00
-  }, 
+  },
+  {
+    id: 8,
+    user_id: 1,
+    description: 'Trader Joes',
+    category_id: 13,
+    timestamp: 1625454527434,
+    cost: 54.25
+  },
+  {
+    id: 9,
+    user_id: 1,
+    description: 'Whole foods',
+    category_id: 13,
+    timestamp: 1628827887766,
+    cost: 122.23
+  },
+  {
+    id: 10,
+    user_id: 1,
+    description: 'bird seed',
+    category_id: 8,
+    timestamp:1630383190117,
+    cost: 8.75
+  },
+  {
+    id: 11,
+    user_id: 1,
+    description: 'kitty litter',
+    category_id: 5,
+    timestamp: 1630383190117,
+    cost: 9.95
+  },
+  {
+    id: 12,
+    user_id: 1,
+    description: 'electric bill',
+    category_id: 12,
+    timestamp: 1626322651283,
+    cost: 33.46
+  },
+  {
+    id: 13,
+    user_id: 1,
+    description: 'electric bill',
+    category_id: 12,
+    timestamp: 1628914726162,
+    cost: 42.55
+  },
+  {
+    id: 14,
+    user_id: 1,
+    description: 'Red Baron pizza x 5',
+    category_id: 2,
+    timestamp: 1627187320151,
+    cost: 25.00
+  },
+  {
+    id: 15,
+    user_id: 1,
+    description: 'Corn dogs',
+    category_id: 2,
+    timestamp:1629088233872,
+    cost: 12.95
+  },
+  {
+    id: 16,
+    user_id: 1,
+    description: 'fish flakes',
+    category_id: 7,
+    timestamp: 1630211540604,
+    cost: 29.99
+  },
+  {
+    id: 17,
+    user_id: 1,
+    description: 'cat food',
+    category_id: 5,
+    timestamp: 1630038830340,
+    cost: 19.95
+  },
+  {
+    id: 18,
+    user_id: 1,
+    description: 'kitty treats',
+    category_id: 5,
+    timestamp: 1629520486190,
+    cost: 12.98
+  },
+  {
+    id: 19,
+    user_id: 1,
+    description: 'family size frozen lasagna x 5',
+    category_id: 2,
+    timestamp: 1629952582873,
+    cost: 52.23
+  },
+  {
+    id: 20,
+    user_id: 1,
+    description: 'Chipotle',
+    category_id: 3,
+    timestamp: 1630471143093,
+    cost: 12.80
+  },
+  {
+    id: 21,
+    user_id: 1,
+    description: 'Panera',
+    category_id: 3,
+    timestamp: 1626928829853,
+    cost: 14.55
+  },
+  {
+    id: 22,
+    user_id: 1,
+    description: 'dog treats',
+    category_id: 6,
+    timestamp: 1629607328280,
+    cost: 9.95
+  },
+  {
+    id: 23,
+    user_id: 1,
+    description: 'Thai food',
+    category_id: 3,
+    timestamp: 1628743619244,
+    cost: 16.35
+  },
+  {
+    id: 24,
+    user_id: 1,
+    description: 'dentist appointment',
+    category_id: 14,
+    timestamp: 1627966361341,
+    cost: 112.00
+  },
+  {
+    id: 25,
+    user_id: 1,
+    description: 'doctor appointment',
+    category_id: 14,
+    timestamp: 1628916828192,
+    cost: 125.00
+  },
+  {
+    id: 26,
+    user_id: 1,
+    description: 'gas',
+    category_id: 15,
+    timestamp: 1628485170299,
+    cost: 34.31
+  }
 ];
--- data/recurringPurchaseItems.js
+++ data/recurringPurchaseItems.js
@@ -6,8 +6,8 @@ module.exports = [
     cost: '$35.90',
     category_id: 9,
     frequency: 2628000000,
-    start_timestamp: 1599069755674,
-    stop_timestamp: null 
+    start_timestamp: 1625454527434,
+    stop_timestamp: null
   },
   {
     id: 2,
@@ -16,7 +16,77 @@ module.exports = [
     cost: '$14.99',
     category_id: 9,
     frequency: 2628000000,
-    start_timestamp: 1614837595129, 
+    start_timestamp: 1625454527434,
     stop_timestamp: 1627977529785
+  },
+  {
+    id: 3,
+    user_id: 1,
+    description: 'mortgage',
+    cost: '$1800.00',
+    category_id: 11,
+    frequency: 2628000000,
+    start_timestamp: 1625454527434,
+    stop_timestamp: null
+  },
+  {
+    id: 4,
+    user_id: 1,
+    description: 'cell phone bill',
+    cost: '$24.99',
+    category_id: 12,
+    frequency: 2628000000,
+    start_timestamp: 1625454527434,
+    stop_timestamp: null
+  },
+  {
+    id: 5,
+    user_id: 1,
+    description: 'The Atlantic',
+    cost: '$79.90',
+    category_id: 9,
+    frequency: 31536000000,
+    start_timestamp: 1625454527434,
+    stop_timestamp: null
+  },
+  {
+    id: 6,
+    user_id: 1,
+    description: 'internet bill',
+    cost: '$60.00',
+    category_id: 12,
+    frequency: 2628000000,
+    start_timestamp: 1628569298041,
+    stop_timestamp: null
+  },
+  {
+    id: 7,
+    user_id: 1,
+    description: 'dollar shave club',
+    cost: '$26.00',
+    category_id: 9,
+    frequency: 1209600000,
+    start_timestamp: 1625459106089,
+    stop_timestamp: null
+  },
+  {
+    id: 8,
+    user_id: 1,
+    description: 'health insurance',
+    cost: '$352.00',
+    category_id: 14,
+    frequency: 2628000000,
+    start_timestamp: 1625460694247,
+    stop_timestamp: null
+  },
+  {
+    id: 9,
+    user_id: 1,
+    description: 'car insurance',
+    cost: '$24.00',
+    category_id: 15,
+    frequency: 2628000000,
+    start_timestamp: 1625461070049,
+    stop_timestamp: null
   }
 ];
