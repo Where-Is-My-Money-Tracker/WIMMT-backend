e5c9dbcabe5285a8c08b08e5d1c70aa1f31ce423
Julie Nisbet <jmnisbet@gmail.com>
2021-09-14 15:59:50 -0700

    DO NOT MERGE -- sample data for bug with sub-cats

--- data/categories.js
+++ data/categories.js
@@ -7,86 +7,45 @@ module.exports = [
   },
   {
     id: 2,
-    parent_id: 1,
-    description: 'Ready meals',
-    user_id: 1
-  },
-  {
-    id: 3,
-    parent_id: 1,
-    description: 'Takeout',
-    user_id: 1
-  },
-  {
-    id: 4,
     parent_id: 0,
     description: 'Pets',
     user_id: 1
   },
   {
+    id: 3,
+    parent_id: 0,
+    description: 'Medical',
+    user_id: 1
+  },
+  {
+    id: 4,
+    parent_id: 1,
+    description: 'Groceries',
+    user_id: 1
+  },
+  {
     id: 5,
-    parent_id: 4,
-    description: 'Cat',
+    parent_id: 1,
+    description: 'Fast Food',
     user_id: 1
   },
   {
     id: 6,
-    parent_id: 4,
-    description: 'Dog',
+    parent_id: 0,
+    description: 'Auto',
     user_id: 1
   },
   {
     id: 7,
     parent_id: 4,
-    description: 'Fish',
+    description: 'Sprouts',
     user_id: 1
   },
   {
     id: 8,
     parent_id: 4,
-    description: 'Canaries',
+    description: 'Trader Joes',
     user_id: 1
   },
-  {
-    id: 9,
-    parent_id: 0,
-    description: 'Subscriptions',
-    user_id: 1
-  },
-  {
-    id: 10,
-    parent_id: 0,
-    description: 'Self improvement',
-    user_id: 1
-  },
-  {
-    id: 11,
-    parent_id: 0,
-    description: 'housing',
-    user_id: 1
-  },
-  {
-    id: 12,
-    parent_id: 0,
-    description: 'bills',
-    user_id: 1
-  },
-  {
-    id: 13,
-    parent_id: 1,
-    description: 'groceries',
-    user_id: 1
-  },
-  {
-    id: 14,
-    parent_id: 0,
-    description: 'medical',
-    user_id: 1
-  },
-  {
-    id: 15,
-    parent_id: 0,
-    description: 'transportation',
-    user_id: 1
-  }
+
 ];
--- data/recurringPurchaseItems.js
+++ data/recurringPurchaseItems.js
@@ -1,80 +1,10 @@
 module.exports = [
   {
-    id: 1,
-    user_id: 1,
-    description: 'eharmony membership',
-    cost: '$35.90',
-    category_id: 9,
-    frequency: 2628000000,
-    start_timestamp: 1625454527434,
-    stop_timestamp: null
-  },
-  {
-    id: 2,
-    user_id: 1,
-    description: 'Scientific American',
-    cost: '$14.99',
-    category_id: 9,
-    frequency: 2628000000,
-    start_timestamp: 1625454527434,
-    stop_timestamp: 1627977529785
-  },
-  {
-    id: 3,
-    user_id: 1,
-    description: 'mortgage',
-    cost: '$1800.00',
-    category_id: 11,
-    frequency: 2628000000,
-    start_timestamp: 1625454527434,
-    stop_timestamp: null
-  },
-  {
-    id: 4,
-    user_id: 1,
-    description: 'cell phone bill',
-    cost: '$24.99',
-    category_id: 12,
-    frequency: 2628000000,
-    start_timestamp: 1625454527434,
-    stop_timestamp: null
-  },
-  {
-    id: 5,
-    user_id: 1,
-    description: 'The Atlantic',
-    cost: '$79.90',
-    category_id: 9,
-    frequency: 31536000000,
-    start_timestamp: 1625454527434,
-    stop_timestamp: null
-  },
-  {
-    id: 6,
-    user_id: 1,
-    description: 'internet bill',
-    cost: '$60.00',
-    category_id: 12,
-    frequency: 2628000000,
-    start_timestamp: 1628569298041,
-    stop_timestamp: null
-  },
-  {
-    id: 7,
-    user_id: 1,
-    description: 'dollar shave club',
-    cost: '$26.00',
-    category_id: 9,
-    frequency: 1209600000,
-    start_timestamp: 1625459106089,
-    stop_timestamp: null
-  },
-  {
     id: 8,
     user_id: 1,
     description: 'health insurance',
     cost: '$352.00',
-    category_id: 14,
+    category_id: 3,
     frequency: 2628000000,
     start_timestamp: 1625460694247,
     stop_timestamp: null
@@ -84,7 +14,7 @@ module.exports = [
     user_id: 1,
     description: 'car insurance',
     cost: '$24.00',
-    category_id: 15,
+    category_id: 6,
     frequency: 2628000000,
     start_timestamp: 1625461070049,
     stop_timestamp: null
